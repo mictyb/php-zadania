093e0752047cab7ac970fff47b23af88a3ce61a5
Michał Tyburczy <122165250+mictyb@users.noreply.github.com>
2023-02-01 17:13:12 +0100

    Create nielamliwaspacja..php
    dodoalem nielamliwa spacje zeby pcwiczyc formatowania tekstu

---
+++ nielamliwaspacja..php
@@ -0,0 +1,22 @@
+?php
+$x = 5;
+$y = 11;
+
+$results = $y % $x;
+
+if ($results == 0){
+echo 'liczba';
+echo $y;
+echo ' jest podzielna przez ';
+echo $x;
+}
+else {
+echo 'liczba';
+echo $y;
+echo ' nie jest podzielna przez ';
+echo $x;
+}
+
+
+echo '<br> nie łamliwa  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' .'spacja';
+?>
